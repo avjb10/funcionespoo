9aeddec3e96cc63b2aca9e7ff58b3626db006c3c
avjb10 <1011092066@teinco.edu.co>
2025-03-26 10:41:24 -0500

    Add files via upload

---
+++ funcionuno.java
@@ -0,0 +1,15 @@
+public class Ejemplo {
+
+    // creacion de la funcion
+
+    public static int sumar(int a, int b){
+        return a+b;
+        
+    }
+
+    //ejecucion de la funcion sumar
+    public static void main(String[] args) {
+        int resultados = sumar (15,333);
+        System.out.println(resultados);
+    }
+}
